93f917dc7b6402eaa5233c2bf17ed9b9e7237572
djordjereact <djole07mihailovic@gmail.com>
2019-10-10 21:48:12 +0200

    added new classes and fixing the app

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = { 
+      count:0
+     };
+
     render() { 
-        return <div><h1>Hello world</h1><button>Increment</button></div>;
+        return <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+            </div>;
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
  
