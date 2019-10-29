e7340c4e990f292c4749e8f0ac829709583ea892
djordjereact <djole07mihailovic@gmail.com>
2019-10-29 21:47:56 +0100

    Add new component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-      count:0,
-      tags: [ 'tag1', 'tag2', 'tag3' ]
+      value: this.props.value
+      
      };
 
 
      handleIncrement = () => {
-        this.setState({ count: this.state.count +1 })
+        this.setState({ value: this.state.value + 1 })
      }
 
     render() { 
@@ -17,17 +17,18 @@ class Counter extends Component {
                 <button 
                 onClick={this.handleIncrement} 
                 className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.handleDelete} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>;
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const { value: count } = this.state;
         return count === 0 ? 'Zero' : count;
     }
 }
